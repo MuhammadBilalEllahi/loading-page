fa8f30e0cf05f22612f53d58ac3fefc60e5978c4
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2023-09-01 02:04:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,14 @@ gsap.fromTo(
     delay: 0.5,
   }
 );
+
+
+// Use the below code if you face any difficulty and delete the above code. 
+
+window.onload = function () {
+  var loadingPage = document.querySelector('.loading-page');
+  loadingPage.style.opacity = 0; // Set opacity to fade out
+  setTimeout(function () {
+    loadingPage.style.display = 'none'; // Hide the loading screen after fading
+  }, 1000); // Adjust the delay based on your preference
+};
